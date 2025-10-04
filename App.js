d747b6b567ae7d0376e0aaa979f0c25078d1bfee
balkrishnamali <balkrishna_mali@rediffmail.com>
2025-10-04 22:11:40 +0530

    React component starting

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
+//const heading = React.createElement("h1", { id: "heading" }, "Hello reactjs");
+
+const heading = (
+  <h1 id="heading" className="head" tabIndex="1">
+    This is JSX code syntax
+  </h1>
+);
+
+console.log(heading);
+//console.log(heading1);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" }, [React.createElement("h1", { id: "childDiv" }, "I am child h1"), React.createElement("h2", { id: "childDiv" }, "I am child h2")]),
-React.createElement("div", { id: "child" }, [React.createElement("h1", { id: "childDiv" }, "I am child h1"), React.createElement("h2", { id: "childDiv" }, "I am child h2")])
-]);
-root.render(parent);
+root.render(heading);
